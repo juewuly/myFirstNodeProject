9b600a094414ac7ad0538f69adc4c367f989f05a
liuyang <liuyang@meiqia.com>
2017-12-28 14:13:02 +0800

    feature: 开始nodeJS

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+    function oneRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
+
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        var content =  route(handle, pathname);
+        response.write(content);
+        response.end();
+    }
+
+    http.createServer(oneRequest).listen(8888);
+    console.log('server has started.');
+}
+
+exports.start = start;
