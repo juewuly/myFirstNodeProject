43f04e8eb6e12f7040eab0b64d9bc0e0764814e4
liuyang <liuyang@meiqia.com>
2017-12-28 15:30:11 +0800

    feature: 非阻塞响应

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received.');
 
+        route(handle, pathname, response);
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var content =  route(handle, pathname);
-        response.write(content);
-        response.end();
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // var content =  route(handle, pathname);
+        // response.write(content);
+        // response.end();
     }
 
     http.createServer(oneRequest).listen(8888);
